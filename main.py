62b35b8f6cab28bbfb8cf8b8bfa2481328d862f5
Saibou <saiboub80@gmail.com>
2023-08-18 19:00:52 -0400

    added statis, templates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_bootstrap5 import Bootstrap
 
 
 
@@ -8,14 +9,40 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 
 
 ########Routes###############
 @app.route('/')
-def home():
-    return render_template("Navbar.html")
+def navbar():
+    return render_template("navbar.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+@app.route("/register")
+def register():
+    render_template('register.html')
+
+@app.route("/footer")
+def footer():
+    return render_template("footer.html")
+
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
+
+
+@app.route("/make-post")
+def make_post():
+    return render_template("make_post.html")
+
 
 
 
