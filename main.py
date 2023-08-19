056a35999be6da96df2d8538760cf0d9f1bae6bf
Saibou <saiboub80@gmail.com>
2023-08-18 23:07:27 -0400

    added Database sqlite

--- main.py
+++ main.py
@@ -1,53 +1,71 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect, url_for
 from flask_bootstrap5 import Bootstrap
+from flask_sqlalchmy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField ,Submitfield
+from wtforms.validations import DataRequired, URL
+from flask_crediton import Ckeditor ,CKeditorField
+from datetime import date
 
 
-
-
-
-#############connections########
 
 
 app = Flask(__name__)
+app.config[SECRET_KEY]= '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
 
+###CONNECT TO DB ####
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///posts.db'
+db= SQLAlchemy()
+db.init_app(app)
 
 
-########Routes###############
+#####Configure Table
+
+class BlogPost(db.Model):
+    id= db.column(db.Integer,primary_key=True)
+    title=db.column(db.String(250),unique=True, nullable=False)
+    subtitle=db.column(db.String(250),nullable=False)
+    date = db.column(db.String(250), nullable=False)
+    body = db.column(db.Text, nullable=False)
+    author=db.column(db.String(250),nullable=False)
+    img_url=db.column(db.String(250),nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+
 @app.route('/')
-def navbar():
-    return render_template("navbar.html")
+def get_all_posts():
+    result= db.session.execute(db.select(BlogPost))
+    posts= result.scalars().all()
+    return render_template("index.html", all_posts=posts)
+
+@app.route('/post/<int:post_id>')
+def Show_post(post_id):
+    requested_post= db.get_or_404(BlogPost,post_id)
+    return render_template('post.html',post=requested_post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html')
-
-
-@app.route("/register")
-def register():
-    render_template('register.html')
-
-@app.route("/footer")
-def footer():
-    return render_template("footer.html")
-
-
-@app.route("/post")
-def post():
-    return render_template("post.html")
-
-
-@app.route("/make-post")
-def make_post():
-    return render_template("make_post.html")
+    return render_template("contact.html")
 
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
+
+
+
+
 
 
 
